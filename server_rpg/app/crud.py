42c59b8a6396878d729e588c772bd19a6b1c7e04
Marek Rymarkiewicz <82097783+MarekRymarkiewicz@users.noreply.github.com>
2022-06-09 15:09:20 +0200

    JSON data transfer; Basic Jinja2 template implementation.

--- server_rpg/app/crud.py
+++ server_rpg/app/crud.py
@@ -6,7 +6,15 @@ def get_players(db):
     query = "SELECT rowid, name, profession, hp, attack_point, status, deaths, kills FROM players"
 
     for player in db.execute(query):
-        player_data = player
+        player_data = {"id": player[0],
+                       "name": player[1],
+                       "profession": player[2],
+                       "hp": player[3],
+                       "attack_point": player[4],
+                       "status": player[5],
+                       "deaths": player[6],
+                       "kills": player[7]
+                       }
 
         # TODO: [[1, Damian],[2,Tomek]]
         # Change code so the result's data structure looks like this:
--- server_rpg/app/main.py
+++ server_rpg/app/main.py
@@ -4,6 +4,9 @@ from starlette.responses import JSONResponse
 from database import cursor
 from crud import get_players
 
+from starlette.templating import Jinja2Templates
+
+templates = Jinja2Templates(directory='templates')
 
 async def players(request):
     with cursor() as cur:
@@ -11,6 +14,14 @@ async def players(request):
     return JSONResponse(results)
 
 
+async def players_table(request):
+    with cursor() as cur:
+        results = get_players(cur)
+    context = {"request": request, "players": results}
+    return templates.TemplateResponse("index.html", context)
+
+
 app = Starlette(routes=[
-    Route('/api/players', players, methods=['GET'])
+    Route('/api/players', players, methods=['GET']),
+    Route('/api/players_table', players_table, methods=['GET'])
 ])
